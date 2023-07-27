545251d8c61910ffa728f52230d2108abeb17194
e23007 <e23007@std.it-college.ac>
2023-07-27 20:08:19 +0900

    2.5 base

---
+++ components/layout.js
@@ -0,0 +1,10 @@
+import Header from 'components/header'
+import Footer from 'components/footer'
+const Layout = ({ children }) => (
+  <>
+    <Header />
+    <main>{children}</main>
+    <Footer />
+  </>
+)
+export default Layout
