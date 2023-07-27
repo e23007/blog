5392304b5b80dc3d7a7111387a4e4bedf34d45d5
e23007 <e23007@std.it-college.ac>
2023-07-27 20:43:31 +0900

    2.6 base

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
-import Layout from 'components/layout'
+// import Layout from 'components/layout'
 import Hero from 'components/hero'
 
 const Home = () => {
   return (
     <>
-      <Layout>
-        <Hero />
-      </Layout>
+      <Hero />
     </>
   )
 }
