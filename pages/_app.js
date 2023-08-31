fde702a9b2e075d9961d1eb19f1e8c172505d4e6
e23007 <e23007@std.it-college.ac>
2023-08-31 21:09:49 +0900

    5.7 fin

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
 import Layout from 'components/layout'
+
+//Font Awsomeの設定
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
+
 const App = ({ Component, pageProps }) => {
   return (
     <Layout>
