acf6d079f6b68f56fad448e920b4fedc463ee3d2
e23007 <e23007@std.it-college.ac>
2023-08-31 21:45:53 +0900

    5.9 fin

---
+++ components/social.js
@@ -0,0 +1,31 @@
+import styles from 'styles/social.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faTwitter,
+  faFacebook,
+  faGithub
+} from '@fortawesome/free-brands-svg-icons'
+
+const Social = () => (
+  <ul className={styles.list}>
+    <li>
+      <a href='https://twitter.com/'>
+        <FontAwesomeIcon icon={faTwitter} />
+        <span className='sr-only'>Twitter</span>
+      </a>
+    </li>
+    <li>
+      <a href='https://www.facebook.com/'>
+        <FontAwesomeIcon icon={faFacebook} />
+        <span className='sr-only'>Facebook</span>
+      </a>
+    </li>
+    <li>
+      <a href='https://gitgub.com/'>
+        <FontAwesomeIcon icon={faGithub} />
+        <span className='sr-only'>Github</span>
+      </a>
+    </li>
+  </ul>
+)
+export default Social
