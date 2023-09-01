5e2b3c9fb1b790e5a3412e206def783a9d717321
e23007 <e23007@std.it-college.ac>
2023-09-01 20:42:50 +0900

    6.4 fin

--- components/meta.js
+++ components/meta.js
@@ -1,10 +1,17 @@
 import Head from 'next/head'
 
-const Meta = ({ pageTitle }) => (
-  <Head>
-    <title>{pageTitle}</title>
-    <meta property='og:title' content={pageTitle} />
-  </Head>
-)
+//サイトに関する情報
+import { siteMeta } from 'lib/constants'
+const { siteTitle, siteDesc, siteUrl, siteLocal, siteType, siteIcon } = siteMeta
 
+const Meta = ({ pageTitle }) => {
+  //ページのタイトル
+  const title = pageTitle ? `${pageTitle}|${siteTitle}` : siteTitle
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta property='og:title' content={title} />
+    </Head>
+  )
+}
 export default Meta
