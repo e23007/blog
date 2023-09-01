dc5b2e3c08892068a747d3c636d879227c0dd308
e23007 <e23007@std.it-college.ac>
2023-09-01 19:57:10 +0900

    6.3 fin

---
+++ components/meta.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+
+const Meta = ({ pageTitle }) => (
+  <Head>
+    <title>{pageTitle}</title>
+    <meta property='og:title' content={pageTitle} />
+  </Head>
+)
+
+export default Meta
