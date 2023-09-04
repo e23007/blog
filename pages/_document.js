214c59a494cba8fb3c4c135a3144fbe6e40951e4
e23007 <e23007@std.it-college.ac>
2023-09-04 18:43:35 +0900

    6.6 fin

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { siteMeta } from 'lib/constants'
+const { siteLang } = siteMeta
+const Document = () => (
+  <Html lang='{siteLang}'>
+    <Head />
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+)
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
-}
+export default Document
