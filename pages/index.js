29385766ac1e139e2acc6f73a2320e87e6eb7536
e23007 <e23007@std.it-college.ac.jp>
2023-10-18 19:00:50 +0900

    cp7 fin

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
 import Hero from 'components/hero'
 import Meta from 'components/meta'
 import Container from 'components/container'
-const Home = () => {
-  return (
-    <Container>
-      <Meta />
-      <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
-    </Container>
-  )
-}
+const Home = props => (
+  <Container>
+    <Hero {...props} />
+  </Container>
+)
+
+export const getStaticProps = async contxt => ({
+  props: {
+    title: 'CUBE',
+    subtitle: 'アウトプットしていくサイト',
+    imageOn: 'true'
+  }
+})
+
 export default Home
 // <meta neme='description' content='アウトプットしていくサイト' />
 //<meta property='og:description' content='アウトプットしていくサイト' />
