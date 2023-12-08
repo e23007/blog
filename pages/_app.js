1c3c36e75d632b7e8b65d9b255b41d4755878093
e23007 <e23007@std.it-college.ac.jp>
2023-12-08 19:00:25 +0900

    10.6 fin

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,33 @@
 import '@/styles/globals.css'
 import Layout from 'components/layout'
+// import Script from 'next/script'
+// import * as gtag from 'lib/grag'
 
-//Font Awsomeの設定
+// Font Awsomeの設定
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false
 
 const App = ({ Component, pageProps }) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {/* <Script
+        strategy='afterInteractive'
+        src={`https://www.googlemanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id='gtag-init'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTMLHTML={{
+          __html: `window.dataLayer=window.dataLayer|| []; function gtag(){dataLayer.push(arguments);}
+    gtag('config','${gtag.GA_MEASUREMENT_ID}')
+    `
+        }}
+      /> */}
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 export default App
